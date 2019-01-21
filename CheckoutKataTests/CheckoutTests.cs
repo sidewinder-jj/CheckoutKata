1a9c6d0b5376933f6c4f1143ffc3a6ab32c7a0fc
Jamie Jordan <jj_sw@outlook.com>
2019-01-21 01:12:43 +0000

    Failing test for returning the unit price when a single item is scanned. I need to pass the list of items into the checkout but I haven't implemented a constructor yet.

--- CheckoutKataTests/CheckoutTests.cs
+++ CheckoutKataTests/CheckoutTests.cs
@@ -1,8 +1,26 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using Xunit;
 
 namespace CheckoutKataTests
 {
     public class CheckoutTests
     {
+        Dictionary<string, int> items = new Dictionary<string, int>
+        {
+            { "A", 50},
+            { "B", 30 },
+            { "C", 20 },
+            { "D", 15 }
+        };
+
+        [Fact]
+        public void GetTotalPrice_ReturnsUnitPrice_OneItemIsScanned()
+        {
+            const string sku = "A";
+
+            int unitPrice = items[sku];
+
+            var checkout = new Checkout(items, discounts);
+        }
     }
 }
