74267802ed87d82acbf2476d7f1719cdd69e1d4b
Jamie Jordan <jj_sw@outlook.com>
2019-01-21 01:36:15 +0000

    Adds test for getting the total when multiple items are scanned. It passes immediately.

--- CheckoutKataTests/CheckoutTests.cs
+++ CheckoutKataTests/CheckoutTests.cs
@@ -29,5 +29,26 @@ namespace CheckoutKataTests
 
             Assert.Equal(unitPrice, result);
         }
+
+        [Fact]
+        public void GetTotalPrice_ReturnsSumOfPrices_MultipleItemsAreScanned()
+        {
+            const string sku1 = "A";
+            const string sku2 = "B";
+            const string sku3 = "D";
+
+            int price1 = items[sku1];
+            int price2 = items[sku2];
+            int price3 = items[sku3];
+            int sum = price1 + price2 + price3;
+
+            var checkout = new Checkout(items);
+
+            checkout.Scan(sku1);
+            checkout.Scan(sku2);
+            checkout.Scan(sku3);
+
+            Assert.Equal(sum, checkout.GetTotalPrice());
+        }
     }
 }
